98979a37e07fec0cd5805d1224c0d9327f55a590
Justin Wasserman <justin@tokkie.onmicrosoft.com>
2022-02-01 17:57:23 +0200

    Added ProductCategory

---
+++ S-StateOnline.Core/Models/PorductCategory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S_StateOnline.Core.Models
+{
+    class PorductCategory
+    {
+        public string Id;
+        public string Category { get; set; }
+        public ProductCategory()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+    }
+}
