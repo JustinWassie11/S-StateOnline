fee88790a1d5ba89d188bfb4b5e2146cc5f9d96b
Justin Wasserman <justin@tokkie.onmicrosoft.com>
2022-02-01 18:24:45 +0200

    Created BaseEntity And InMemoryRepository

---
+++ S-StateOnline.DataAccess.Inmemory/InMemoryRepository.cs
@@ -0,0 +1,66 @@
+﻿using S_StateOnline.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S_StateOnline.DataAccess.Inmemory
+{
+    class InMemoryRepository<T> where T: BaseEntity
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<T> items;
+        string classname;
+        public InMemoryRepository()
+        {
+            classname = typeof(T).Name;
+            items = cache["classname"] as List<T>;
+            if (items == null)
+                items = new List<T>();
+        }
+        public void Commit()
+        {
+            cache["classname"] = items;
+        }
+        public void Insert(T t)
+        {
+            items.Add(t);
+        }
+        public void Update(T t)
+        {
+            T tToUpdate = items.Find(i => i.Id == t.Id);
+            if (tToUpdate != null)
+                tToUpdate = t;
+            else
+            {
+                throw new Exception(classname + " Not Found");
+            }
+        }
+        public T Find(string Id)
+        {
+            T t = items.Find(i => i.Id == Id);
+            if (t != null)
+                return t;
+            else
+            {
+                throw new Exception(classname + " Not Found");
+            }
+        }
+        public IQueryable<T> Collection()
+        {
+            return items.AsQueryable();
+        }
+        public void Delete(string Id)
+        {
+            T tToDelete = items.Find(i => i.Id == Id);
+            if (tToDelete != null)
+                items.Remove(tToDelete);
+            else
+            {
+                new Exception(classname + " Not Found");
+            }
+        }
+    }
+}
